210384186b7b93f7609b41acb1e9369fa702fd5f
AvinashMarkad <markadavi3719@gmail.com>
2025-01-13 22:54:15 +0530

    backend creates

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT || 8000;
+
+app.get('/ping', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
